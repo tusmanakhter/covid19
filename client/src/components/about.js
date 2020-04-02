7f9fd7f2c1f68cfafb4c65719155ce9e836131e3
Tusman Akhter <tusman96@hotmail.com>
2020-04-02 00:10:46 -0400

    Added about page  styling

--- client/src/components/about.js
+++ client/src/components/about.js
@@ -1,24 +1,21 @@
 import React from 'react'
 import Layout from './layout'
-import { EuiPanel, EuiText, EuiFlexGroup, EuiFlexItem } from '@elastic/eui';
+import { EuiPanel, EuiText } from '@elastic/eui';
+import './about.css';
 
 const About = () => {
   return (
     <Layout>
-      <EuiPanel>
-        <EuiFlexGroup direction="column">
-          <EuiFlexItem>
-            <EuiText textAlign="center">
-              <h2>About</h2>
-              <p>This website was created by <a target="_blank" rel="noopener noreferrer" href="https://www.tusmanakhter.com/">Tusman Akhter</a>. Show your support on <a target="_blank" rel="noopener noreferrer" href="https://ko-fi.com/tusmanakhter">Ko-Fi</a>.</p>
-              <p>Contact <a target="_blank" rel="noopener noreferrer" href="mailto:dev@trackingcovid.info?Subject=Covid%20Tracker">dev@trackingcovid.info</a> for any suggestions, bugs or issues.</p>
-              <h2>Sources</h2>
-              <p>Global Data provided by <a target="_blank" rel="noopener noreferrer" href="https://systems.jhu.edu/">JHU CSSE</a> on <a target="_blank" rel="noopener noreferrer" href="https://github.com/CSSEGISandData/COVID-19">Github</a></p>
-              <p>Montreal data provided by <a target="_blank" rel="noopener noreferrer" href="https://santemontreal.qc.ca/en/public/coronavirus-covid-19/">Santé Montréal</a></p>
-              <p>Icon provided by <a target="_blank" rel="noopener noreferrer" href="https://www.iconfinder.com/justicon">Just Icon</a> licensed under <a target="_blank" rel="noopener noreferrer" href="https://creativecommons.org/licenses/by/3.0/">CC BY 3.0</a></p>
-            </EuiText>
-          </EuiFlexItem>
-        </EuiFlexGroup>
+      <EuiPanel className="panel">
+        <EuiText textAlign="center">
+          <h2>About</h2>
+          <p>This website was created by <a target="_blank" rel="noopener noreferrer" href="https://www.tusmanakhter.com/">Tusman Akhter</a>. Show your support on <a target="_blank" rel="noopener noreferrer" href="https://ko-fi.com/tusmanakhter">Ko-Fi</a>.</p>
+          <p>Contact <a target="_blank" rel="noopener noreferrer" href="mailto:dev@trackingcovid.info?Subject=Covid%20Tracker">dev@trackingcovid.info</a> for any suggestions, bugs or issues.</p>
+          <h2>Sources</h2>
+          <p>Global Data provided by <a target="_blank" rel="noopener noreferrer" href="https://systems.jhu.edu/">JHU CSSE</a> on <a target="_blank" rel="noopener noreferrer" href="https://github.com/CSSEGISandData/COVID-19">Github</a></p>
+          <p>Montreal data provided by <a target="_blank" rel="noopener noreferrer" href="https://santemontreal.qc.ca/en/public/coronavirus-covid-19/">Santé Montréal</a></p>
+          <p>Icon provided by <a target="_blank" rel="noopener noreferrer" href="https://www.iconfinder.com/justicon">Just Icon</a> licensed under <a target="_blank" rel="noopener noreferrer" href="https://creativecommons.org/licenses/by/3.0/">CC BY 3.0</a></p>
+        </EuiText>
       </EuiPanel>
     </Layout>
   )
