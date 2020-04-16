fbb09078f8a36ebf47575e579040f14687e1527c
Tusman Akhter <tusman96@hotmail.com>
2020-04-16 00:36:42 -0400

    Updates

---
+++ icon.tokens-tokenFlattened-js.render-page.js
@@ -0,0 +1,53 @@
+exports.ids = ["icon.tokens-tokenFlattened-js"];
+exports.modules = {
+
+/***/ "./node_modules/@elastic/eui/es/components/icon/assets/tokens/tokenFlattened.js":
+/*!**************************************************************************************!*\
+  !*** ./node_modules/@elastic/eui/es/components/icon/assets/tokens/tokenFlattened.js ***!
+  \**************************************************************************************/
+/*! exports provided: icon */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "icon", function() { return icon; });
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react */ "react");
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_0__);
+function _extends() { _extends = Object.assign || function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; }; return _extends.apply(this, arguments); }
+
+function _objectWithoutProperties(source, excluded) { if (source == null) return {}; var target = _objectWithoutPropertiesLoose(source, excluded); var key, i; if (Object.getOwnPropertySymbols) { var sourceSymbolKeys = Object.getOwnPropertySymbols(source); for (i = 0; i < sourceSymbolKeys.length; i++) { key = sourceSymbolKeys[i]; if (excluded.indexOf(key) >= 0) continue; if (!Object.prototype.propertyIsEnumerable.call(source, key)) continue; target[key] = source[key]; } } return target; }
+
+function _objectWithoutPropertiesLoose(source, excluded) { if (source == null) return {}; var target = {}; var sourceKeys = Object.keys(source); var key, i; for (i = 0; i < sourceKeys.length; i++) { key = sourceKeys[i]; if (excluded.indexOf(key) >= 0) continue; target[key] = source[key]; } return target; }
+
+
+
+var EuiIconTokenFlattened = function EuiIconTokenFlattened(_ref) {
+  var title = _ref.title,
+      titleId = _ref.titleId,
+      props = _objectWithoutProperties(_ref, ["title", "titleId"]);
+
+  return react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("svg", _extends({
+    width: 16,
+    height: 16,
+    viewBox: "0 0 16 16",
+    xmlns: "http://www.w3.org/2000/svg",
+    "aria-labelledby": titleId
+  }, props), title ? react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("title", {
+    id: titleId
+  }, title) : null, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("path", {
+    fillRule: "evenodd",
+    d: "M3.25 3c-.13807 0-.25.11193-.25.25v2c0 .13807.11193.25.25.25h9.5c.1381 0 .25-.11193.25-.25v-2c0-.13807-.1119-.25-.25-.25h-9.5zM3.25 6.75c-.13807 0-.25.11193-.25.25v2c0 .13807.11193.25.25.25H5.5c.13807 0 .25-.11193.25-.25V7c0-.13807-.11193-.25-.25-.25H3.25zM3 10.75c0-.1381.11193-.25.25-.25H5.5c.13807 0 .25.1119.25.25v2c0 .1381-.11193.25-.25.25H3.25c-.13807 0-.25-.1119-.25-.25v-2zM6.31059 10.023c-.08262-.07288-.08262-.22353 0-.29598l3.05348-2.68329c.05353-.04715.12788-.05711.19075-.0253.06322.03182.1031.09928.1031.17327v1.34164L12.819 8.5169c.0992 0 .1796.08587.1796.19167v2.29993c0 .1058-.0804.1917-.1796.1917l-3.16108.0164v1.3417c0 .074-.03988.1414-.1031.1732-.06287.0319-.13722.0219-.19075-.0249L6.31059 10.023z"
+  }));
+};
+
+var icon = EuiIconTokenFlattened;
+EuiIconTokenFlattened.__docgenInfo = {
+  "description": "",
+  "methods": [],
+  "displayName": "EuiIconTokenFlattened"
+};
+
+/***/ })
+
+};;
+//# sourceMappingURL=icon.tokens-tokenFlattened-js.render-page.js.map
---
+++ styles-f7c41c90d2a5e05b2de0.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[1],{"0/2z":function(n,o,c){},"7pQb":function(n,o,c){},"8ypT":function(n,o,c){},CeQG:function(n,o,c){},Ho1l:function(n,o,c){},IsLF:function(n,o,c){},RON8:function(n,o,c){},UNLG:function(n,o,c){},avuP:function(n,o,c){},mqps:function(n,o,c){}}]);
+//# sourceMappingURL=styles-f7c41c90d2a5e05b2de0.js.map
